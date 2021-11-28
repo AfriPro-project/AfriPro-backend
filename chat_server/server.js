cc4fb2410259a34cbd62ec854b7b76f51caefc2a
Adolf <adolfokrah@gmail.com>
2021-11-28 00:35:22 +0000

    update

--- chat_server/server.js
+++ chat_server/server.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3000, function() {
+server.listen(process.env.PORT || 5000, function() {
   console.log("Listening!");
 });
 
